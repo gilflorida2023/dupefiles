0a26d7d35677183fd17b265c5737db4ff30429f7
gilflorida2023 <gilflorida2023@gmail.com>
2024-10-25 11:50:21 -0500

    updated rust documentation in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
-/*
-  Finds all duplicate files in a specified sub-directory tree specified on command-line.
-  
-  Create empty hash table.
-  For each file in the directory tree, 
-      Create an entry in a hash table which is key'ed by sha256 of the file. 
-          Associate with each key :Store absolute path and filename.
-          if the hash already exists, 
-              print a duplicate report consisting of :
-                  dupe1.filename, dupe1.filesize,dupe2.filename,dupe2.filesize
-Note: hidden files, hidden directories and zero byte files are skipped.
-*/
+//! Find all duplicate files in a specified sub-directory tree specified on command-line.
+//! Note: hidden files, hidden directories and zero byte files are skipped.
+//!
+//! # Usage
+//!
+//! ```
+//! $ cargo run -- <input_directory>
+//! ```
+//!
+//! Results will be printed to stdout.
 
 use std::collections::HashMap;
 use std::{env,fs};
 use std::path::{Path, PathBuf};
+use std::io::{Error,ErrorKind};
+use std::os::unix::fs::MetadataExt;
 use anyhow::{Context, Result};
 use walkdir::WalkDir;
 use dupefiles::{get_file_size,compute_sha256,is_hidden};
-use std::io::{Error,ErrorKind};
-use std::os::unix::fs::MetadataExt;
 
+/// This function takes a Path value and returns the inode and device id from the Metadata..
+/// It considers file size, sha256sum, and inode, deviceid to determine if the two paths are duplicates
+/// of each other, or the same file.
+/// 
+/// # Arguments
+///
+/// * `file1` - The first standard file Path
 fn get_file_info(path: &Path) -> std::io::Result<(u64, u64)> {
     let metadata = fs::metadata(path)?;
     
@@ -29,7 +34,20 @@ fn get_file_info(path: &Path) -> std::io::Result<(u64, u64)> {
     Ok((inode, device_id))
 }
 
-fn is_duplicate(file1: &Path,file2: &Path) -> bool {
+/// This function takes two Path values and returns boolean indicating whether the files are duplicates.
+/// It considers file size, sha256sum, and inode, deviceid to determine if the two paths are duplicates
+/// of each other, or the same file.
+/// 
+/// # Arguments
+///
+/// * `file1` - The first standard file Path
+/// * `file2` - The second standard file Path
+///
+/// # Returns
+///
+/// bool reflecting whether the two paths are duplicates.
+///
+fn is_duplicate_file(file1: &Path,file2: &Path) -> bool {
 
     // validate two files exist
     if ! file1.try_exists().unwrap() || ! file2.try_exists().unwrap() {
@@ -51,35 +69,41 @@ fn is_duplicate(file1: &Path,file2: &Path) -> bool {
         return false;
     }
 
+    // vereify two files have same hash
     let f1hash = compute_sha256::compute_sha256(file1).with_context(|| format!("Failed to compute hash for {}", file1.display())).unwrap();
     let f2hash = compute_sha256::compute_sha256(file2).with_context(|| format!("Failed to compute hash for {}", file2.display())).unwrap();
     
     if f1hash != f2hash {
         return false;   
     }
-
+    // dont be tricked by external devices or hardlinks
     let (f1inode, f1device_id) = get_file_info(file1).unwrap();
     let (f2inode, f2device_id) = get_file_info(file2).unwrap();
 
+    // if two alleged files share same device id and inode, 
+    //its  really only one file.
     if  f1device_id == f2device_id && f1inode == f2inode {
         return false;
     }
+
+    // looks like a duplicate file. safe to delete one..
     true
 }
 
+/// This function takes a directory Path value and prints to stdout, a csv file indicating duplicates identified.
+/// It skips zero byte files as well as hidden files and hidden directories. It calls ['crate::dupefiles::is_duplicate_file'] to make sure 
+/// its safe to delete  one copy of identified duplicate. Prints to stdout.
+/// 
+/// # Arguments
+///
+/// * `directory` - The directory Path where the search for duplicates begins.
+///
+/// # Returns
+///
+/// Result
+///
 fn find_duplicates(directory: &Path) -> Result<()> {
-    /*
-    Create empty hash table.
-    For each file in the directory tree, 
-      Note: hidden files, hidden directories and zero byte files are skipped.
-      Create an entry in a hash table which is key'ed by sha256 of the file. 
-          Associate with each key :Store absolute path and filename.
-          if the hash already exists, 
-              print a duplicate report consisting of :
-                  dupe1.filename, dupe1.filesize,dupe2.filename,dupe2.filesize
-     */ 
-    static mut HEADER_PRINTED: bool = false;
-
+    static mut HEADER_PRINTED_ONCE: bool = false;
     let mut hash_map: HashMap<String, PathBuf> = HashMap::new();
 
     for entry in WalkDir::new(directory).into_iter().filter_map(|e| e.ok()) {
@@ -102,15 +126,14 @@ fn find_duplicates(directory: &Path) -> Result<()> {
                     Err(_e) => 0,
                 };
 
-
-                if ! is_duplicate(&existing_path,&path) {
+                if ! is_duplicate_file(&existing_path,&path) {
                     continue;
                 }
 
-                unsafe {
-                    if ! HEADER_PRINTED {
+                unsafe { // ony print header once
+                    if ! HEADER_PRINTED_ONCE {
                         println!("DUPE1.NAME,DUPE1.SIZE,DUPE2.NAME,DUPE2.SIZE");
-                        HEADER_PRINTED = true;
+                        HEADER_PRINTED_ONCE = true;
                     }
                 }
                 println!("\"{}\",{},\"{}\",{}", existing_path.display(),existing_fsize,path.display(),fsize);
@@ -123,6 +146,18 @@ fn find_duplicates(directory: &Path) -> Result<()> {
     Ok(())
 }
 
+/// The main entry point for the program dupefiles.
+///
+/// # Arguments
+///
+/// The program expects a command-line arguments:
+/// * argument directiry is the input file path (required)
+///
+/// # Errors
+///
+/// This function will return an error if:
+/// * The required input file argument is missing
+/// * The input file cannot be read
 fn main() -> Result<(),Error> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
@@ -138,7 +173,6 @@ fn main() -> Result<(),Error> {
     eprintln!("Success");
     Ok(())
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -163,7 +197,7 @@ mod tests {
         fs::hard_link(&file_path, &link_path).unwrap();
     
         // Call the duplicate detection function
-        let result = is_duplicate(&file_path,&link_path);
+        let result = is_duplicate_file(&file_path,&link_path);
     
         // Assert that no duplicates are detected since they point to the same inode
         assert_eq!(result, false, "Should not detect duplicates for hard links");
